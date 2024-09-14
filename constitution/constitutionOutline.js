a66163fa28cafc0057909ca8d65e49886678bcb7
Miranda Zheng <123515762+mirmirmirr@users.noreply.github.com>
2024-09-14 09:43:03 -0400

    Landing Page CSS
    Finished working on the initial landing page.
    
    ConstitutionOutline.js is responsible for loading all articles and amendments from the json file.

---
+++ constitution/constitutionOutline.js
@@ -0,0 +1,60 @@
+document.addEventListener("DOMContentLoaded", function() {
+    fetch('constitution.json')
+        .then(response => response.json())
+        .then(data => {
+            const articles = data.articles;
+            const displayArticles = document.getElementById("articles");
+
+            if (articles) {
+                articles.forEach((article, index) => {
+                    const articleElement = document.createElement("div");
+                    const articleNumber = document.createElement("h4");
+                    const articleTitle = document.createElement("a");
+
+                    articleNumber.innerHTML = article.articleNumber;
+                    articleTitle.innerHTML = article.articleTitle;
+                    articleTitle.href = `articles/index.html?article=${index + 1}`;
+
+                    articleElement.onclick = () => {
+                        window.location.href = articleTitle.href;
+                    };
+                    articleElement.style.cursor = "pointer";
+
+                    articleElement.appendChild(articleNumber);
+                    articleElement.appendChild(articleTitle);
+
+                    displayArticles.appendChild(articleElement);
+                });
+            } else {
+                displayArticles.textContent = "Article not found!";
+            }
+
+            const amendments = data.amendments;
+            const displayAmendments = document.getElementById("amendments");
+
+            if (amendments) {
+                amendments.forEach((amendment, index) => {
+                    const amendmentElement = document.createElement("div");
+                    const amendmentNumber = document.createElement("h4");
+                    const amendmentTitle = document.createElement("a");
+
+                    amendmentNumber.innerHTML = amendment.amendNumber;
+                    amendmentTitle.innerHTML = amendment.amendTitle;
+                    amendmentTitle.href = `amendments/index.html?amendment=${index + 1}`;
+
+                    amendmentElement.onclick = () => {
+                        window.location.href = amendmentTitle.href;
+                    };
+                    amendmentElement.style.cursor = "pointer";
+
+                    amendmentElement.appendChild(amendmentNumber);
+                    amendmentElement.appendChild(amendmentTitle);
+
+                    displayAmendments.appendChild(amendmentElement);
+                });
+            } else {
+                displayAmendments.textContent = "Amendment not found!";
+            }
+        })
+        .catch(error => console.error("Error fetching JSON data:", error));
+});
