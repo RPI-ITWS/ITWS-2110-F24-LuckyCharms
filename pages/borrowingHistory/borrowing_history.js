835ce17af5e89b006984b77b5c285d5d4e39b741
CMarink <christianm7765@gmail.com>
2024-10-14 12:57:58 -0400

    Added button functions
    Added functions to the tabbar buttons
    Also added styling around the item status of each item.

---
+++ pages/borrowingHistory/borrowing_history.js
@@ -0,0 +1,45 @@
+function currentlyBorrowingClick() {
+    const current = document.getElementById('chosen');
+
+    if (current) {
+        current.removeAttribute('id');
+    }
+
+    const tabBar = document.getElementById('tab-bar');
+    const tabBarButtons = tabBar.querySelectorAll('h2');
+    const currentlyBorrowingButton = tabBarButtons[0];
+
+    currentlyBorrowingButton.id = 'chosen';
+}
+
+function borrowingHistoryClick() {
+    const current = document.getElementById('chosen');
+
+    if (current) {
+        current.removeAttribute('id');
+    }
+
+    const tabBar = document.getElementById('tab-bar');
+    const tabBarButtons = tabBar.querySelectorAll('h2');
+    const borrowingHistoryButton = tabBarButtons[1];
+
+    borrowingHistoryButton.id = 'chosen';
+}
+
+function colorItemStatus() {
+    const itemStatusList = document.querySelectorAll('.status');
+
+    itemStatusList.forEach((status) => {
+        if (status.textContent === "OVERDUE - RETURN NOW") {
+            status.style.backgroundColor = 'red';
+        }
+        else if (status.textContent === "Borrowable - No Return") {
+            status.style.backgroundColor = 'green';
+        }
+        else if (status.textContent !== "") {
+            status.style.backgroundColor = 'darkgray';
+        }
+    });
+}
+
+document.addEventListener("DOMContentLoaded", colorItemStatus);
