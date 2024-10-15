ff693bbfcae54a6e14e0a63ceef0490abec6a72e
BradsherC <bradsc@rpi.edu>
2024-10-15 14:14:53 -0400

    button fixes

--- homepage.js
+++ homepage.js
@@ -1,13 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Log In</title>
-  <link rel="stylesheet" href="/resources/style.css" media="screen">
-  <script src="https://code.jquery.com/jquery-3.7.1.js" integrity="sha256-eKhayi8LEQwp4NKxN+CfCh+3qOVUtJn3QNZ0TciWLP4=" crossorigin="anonymous"></script>
-  <script>
-    /*For the general homepage*/
+/*For the general homepage*/
 
 /*Go to the homepage*/
 function homePage(event) {
@@ -118,61 +109,3 @@ document.addEventListener('DOMContentLoaded', function() {
       gettingStartedButton.style.cursor = 'default';
   });
 });
-    // If there's no account with the username, tell them and do nothing; if
-    // there is one, tell them if the password's wrong, or set userid cookie
-    // with the record id and redirect to appropriate homepage.
-    function validateLogin(event){
-      event.preventDefault();
-      $.getJSON("data.json", function(data){
-        for(let x of data.users){
-          if(x.username === $("#username").val()){
-            if(x.password !== $("#pass").val()){
-              alert("Incorrect password.");
-              $("#pass").css("border", "2px solid red");
-              return false;
-            } else {
-              document.cookie = `userid=${x.id}`;
-              if(x.is_admin === 1){
-                document.cookie = `admin=1`;
-                location.replace('./admin_homepage.html');
-                return true;
-              } else {
-                document.cookie = `admin=0`;
-                location.replace("./user_homepage.html");
-                return true;
-              }
-            }
-          }
-        }
-        alert("Account does not exist.");
-        location.replace('./signup.html');
-        return false;
-    });
-  }
-  </script>
-</head>
-<body>
-  <div id="navbar">
-    <h1 id="logo">LIMBS</h1>
-    <h2 class="nav-text" id="featuresButton">Features</h2>
-    <h2 class="nav-text" id="aboutUsButton">About Us</h2>
-    <button class="nav-button" id="login-button">Login</button>
-    <button class="nav-button" id="sign-up-button">Sign Up</button>
-</div>
-  <div id="content">
-    <div class="login_signup">
-      <h4 class="first-letter">Log In</h4>
-      <form id="login" onsubmit="return validateLogin(event);">
-        <label for="username">username</label>
-        <input name="username" id="username" type="text" required placeholder="type username here">
-        <label for="pass">password</label>
-        <input name="pass" id="pass" type="password" required>
-        <!--Inline styling because this is the only page with something like this.-->
-        <a style="text-align: right; margin-top: 11px; font-size: 12px; font-weight: bold;">Forgot Password?</a>
-        <button id="submit" type="submit">Log In</button>
-      </form>
-      <a href="signup.html">Sign Up</a>
-    </div>
-  </div>
-</body>
-</html>
