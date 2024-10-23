ce92052b11e08eaf38ea1646224d952584f6d13f
kellieng <122571116+kellieng@users.noreply.github.com>
2024-10-23 15:37:48 -0400

    initial upload of itemlist.php

---
+++ backend/queries/itemlist.php
@@ -0,0 +1,17 @@
+<?php
+    require "db.php";
+
+    // location name is hte location the user clicks on
+    $location_name = $_GET["location_name"];
+    $query = $db->prepare("SELECT * FROM items WHERE location_name = ?;");
+    $query->bind_param("s", $location_name);
+    $query->execute();
+    $result = $query->get_result();
+    
+    $validItems = array();
+    while ($row = $result->fetch_assoc()) {
+        $validItems[] = $row;
+    }
+
+    print_r(json_encode($validItems));
+?>
