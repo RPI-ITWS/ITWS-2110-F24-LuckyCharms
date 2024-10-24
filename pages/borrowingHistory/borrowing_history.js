a377c5ce6dabc082c3a557046e6599a6dc88fc6b
BradsherC <bradsc@rpi.edu>
2024-10-24 13:48:03 -0400

    Login Cookie Fix; Complete Borrowing History
    For users, Currently Borrowing shows their unreturned items, and Borrowing History shows all items they've ever taken. For admins, Currently Borrowing shows unreturned items in all of their labs and Borrowing History shows all items ever taken from their labs and who took them. Now when the login form is submitted, it deletes their old cookies before creating the new ones.

--- pages/borrowingHistory/borrowing_history.js
+++ pages/borrowingHistory/borrowing_history.js
@@ -11,7 +11,7 @@ function borrowingHistoryClick() {
 }
 
 function logoutClick() {
-    window.location.href = '../login_and_signup_page.html';
+    window.location.href = '../login.html';
 }
 
 function currentlyBorrowingClick() {
@@ -26,6 +26,8 @@ function currentlyBorrowingClick() {
     const currentlyBorrowingButton = tabBarButtons[0];
 
     currentlyBorrowingButton.id = 'chosen';
+    
+    populateHistory();
 }
 
 function borrowingHistoryClick() {
@@ -40,6 +42,8 @@ function borrowingHistoryClick() {
     const borrowingHistoryButton = tabBarButtons[1];
 
     borrowingHistoryButton.id = 'chosen';
+
+    populateHistory();
 }
 
 function colorItemStatus() {
@@ -58,20 +62,28 @@ function colorItemStatus() {
     });
 }
 
-$(document).ready(() => {
-    //If the userid or admin cookies aren't set, or if they are not an admin, redirect to login page
-    if(document.cookie.indexOf('userid=')===-1 || document.cookie.indexOf('admin=')===-1 ||
-        document.cookie.substring(document.cookie.indexOf('admin=')+6)!=="0"){
-      location.href="../../login.html";
+function populateHistory(){
+    if(document.cookie.substring(document.cookie.indexOf('admin=')+6)==="1"){
+        populateAdminHistory();
+    } else {
+        populateUserHistory()
     }
-    let id = parseInt(document.cookie.substring(7, document.cookie.indexOf(";")));
-    $.getJSON("../../data.json", function(dat) {
+}
 
+function populateUserHistory(){
+    $("#table-head").html("<th>Item Name</th><th>Lab/Place</th><th>Status</th><th>Reservation Date</th><th>Expected Return Date</th>");
+    let id = parseInt(document.cookie.substring(7, document.cookie.indexOf(";")));
+
+    $.getJSON("../../data.json", function(dat) {
+        let itemList = "";
         for(let x of dat.reservations){
-            if(x.date_returned === null && x.user_id===id){
+            if((x.date_returned === null || $("#chosen").html() === "Borrowing History") && x.user_id===id){
                 let stat;
-                if(dat.items[x.item_id].borrowable===0){
+                if(dat.items[x.item_id-1].borrowable===0){
                     stat = "Borrowable - No Return";
+                } else if(x.date_returned!==null){
+                    let ret = new Date(x.date_returned);
+                    stat = `Returned ${ret}`;
                 } else if(x.expected_return===null){
                     stat = "No Expected Return Date";
                 } else if(new Date() > new Date(x.expected_return)){
@@ -81,29 +93,82 @@ $(document).ready(() => {
                 }
                 let res = new Date(x.date_reserved);
                 let exp_ret = new Date(x.expected_return);
-                $("tbody").append(`<tr><td class="item-name">${dat.items[x.item_id].name}</td><td class="place">${dat.locations[dat.items[x.item_id].location_id].name}</td><td><span class="status">${stat}</span></td><td class="reservation">${res.toUTCString()}</td><td class="expected-return-date">${x.expected_return===null ? "None" : exp_ret.toUTCString()}</td></tr>`);
+                itemList += `<tr><td class="item-name">${dat.items[x.item_id-1].name}</td><td class="place">${dat.locations[dat.items[x.item_id-1].location_id-1].name}</td><td><span class="status">${stat}</span></td><td class="reservation">${res.toUTCString()}</td><td class="expected-return-date">${x.expected_return===null ? "None" : exp_ret.toUTCString()}</td></tr>`;
             }
         }
-    
+        
+        $("tbody").html(itemList);
+
         colorItemStatus();
-    
-        const menuButton = document.getElementById('menu-icon');
-        const dropdownContent = document.querySelector('.dropdown-content');
-    
-        menuButton.addEventListener('click', function() {
-            dropdownContent.classList.toggle('show');
-        });
-    
-        window.addEventListener('click', function(event) {
-            if (!event.target.matches('#menu-icon') && 
-                !event.target.matches('.dropdown-content') &&
-                !event.target.matches('h3') && 
-                !event.target.matches('h4') && 
-                !event.target.matches('h5')) {
-                if (dropdownContent.classList.contains('show')) {
-                    dropdownContent.classList.remove('show');
-                }
-            }
-        });
     });
+}
+
+function populateAdminHistory(){
+    $("#table-head").html("<th>Item Name</th><th>Borrower Name</th><th>Lab/Place</th><th>Status</th><th>Reservation Date</th><th>Expected Return Date</th>");
+    let id = parseInt(document.cookie.substring(7, document.cookie.indexOf(";")));
+
+    $.getJSON("../../data.json", function(dat) {
+        let itemList = "";
+        let allowedLabs=[];
+        for(let pair of dat.allowed_user_locations){
+            if (pair.user_id == id){
+                allowedLabs.push(pair.location_id);
+            }
+        }
+
+        for(let x of dat.reservations){
+            if((x.date_returned === null || $("#chosen").html() === "Borrowing History") && allowedLabs.includes(dat.items[x.item_id-1].location_id)){
+                let stat;
+                if(dat.items[x.item_id-1].borrowable===0){
+                    stat = "Borrowable - No Return";
+                } else if(x.date_returned!==null){
+                    let ret = new Date(x.date_returned);
+                    stat = `Returned ${ret}`;
+                } else if(x.expected_return===null){
+                    stat = "No Expected Return Date";
+                } else if(new Date() > new Date(x.expected_return)){
+                    stat = "OVERDUE - RETURN NOW";
+                } else {
+                    stat = "Not Due Yet";
+                }
+                let res = new Date(x.date_reserved);
+                let exp_ret = new Date(x.expected_return);
+                itemList += `<tr><td class="item-name">${dat.items[x.item_id-1].name}</td><td class="borrower-name">${dat.users[x.user_id-1].username}</td><td class="place">${dat.locations[dat.items[x.item_id-1].location_id-1].name}</td><td><span class="status">${stat}</span></td><td class="reservation">${res.toUTCString()}</td><td class="expected-return-date">${x.expected_return===null ? "None" : exp_ret.toUTCString()}</td></tr>`;
+            }
+        }
+        
+        $("tbody").html(itemList);
+
+        colorItemStatus();
+    });
+}
+
+$(document).ready(() => {
+    
+    
+    const menuButton = document.getElementById('menu-icon');
+    const dropdownContent = document.querySelector('.dropdown-content');
+
+    menuButton.addEventListener('click', function() {
+        dropdownContent.classList.toggle('show');
+    });
+
+    window.addEventListener('click', function(event) {
+        if (!event.target.matches('#menu-icon') && 
+            !event.target.matches('.dropdown-content') &&
+            !event.target.matches('h3') && 
+            !event.target.matches('h4') && 
+            !event.target.matches('h5')) {
+            if (dropdownContent.classList.contains('show')) {
+                dropdownContent.classList.remove('show');
+            }
+        }
+    });
+
+    //If the userid or admin cookies aren't set, redirect to login page
+    if(document.cookie.indexOf('userid=')===-1 || document.cookie.indexOf('admin=')===-1){
+      location.href="../../login.html";
+    }
+    
+    populateHistory();
 });
