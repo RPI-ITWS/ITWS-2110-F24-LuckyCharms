a3f59354c576218d34d88185e69dc22be2058b70
CMarink <christianm7765@gmail.com>
2024-11-09 16:59:17 -0500

    Added form character limits
    Added character limits to inputs in the add item form. This is so that no one overloads the database and crashes the system.

--- resources/add_form.js
+++ resources/add_form.js
@@ -10,26 +10,29 @@ document.addEventListener('DOMContentLoaded', function () {
     addForm.addEventListener('submit', function (event) {
         event.preventDefault();
 
-        const formContainer = document.getElementById('add-form');
-        formContainer.style.display = "none";
-
-        // Make sure the user is an admin here
-
         const itemName = document.getElementById('item-name').value;
         const itemType = document.getElementById('type-dropdown').value;
         const itemDescription = document.getElementById('new-item-description').value;
         const itemStock = document.getElementById('item-quantity').value;
         const itemImage = document.getElementById('item-image').value;
 
-        // Fetch and POST to PHP here
+        if (itemName.length > 50) {
+            alert("The item name must be less than 100 charecters!")
+        }
+        else {
+            const formContainer = document.getElementById('add-form');
+            formContainer.style.display = "none";
 
-        console.log("Item Name: ", itemName);
-        console.log("Item Type: ", itemType);
-        console.log("Item Description: ", itemDescription);
-        console.log("Item Stock: ", itemStock);
-        console.log("Item Image: ", itemImage);
+            // Fetch and POST to PHP here
 
-        addForm.reset();
+            console.log("Item Name: ", itemName);
+            console.log("Item Type: ", itemType);
+            console.log("Item Description: ", itemDescription);
+            console.log("Item Stock: ", itemStock);
+            console.log("Item Image: ", itemImage);
+
+            addForm.reset();
+        }
     });
 
     const cancelAddFormButton = document.getElementById('cancel-add-form-button');
