ebaeafb3b69704ab77cc5654392da7912d556f10
BradsherC <bradsc@rpi.edu>
2024-11-13 12:04:14 -0500

    Combined and separated various JS functions
    delete.js and add_form.js were combined into admin.js in the admin folder, since they are only used by the admin page. checkout_form.js and lablist.js were combined, since they are each only used by the admin and user homepages, and those pages require both of them, and the userLabs function was moved there from resources/script.js

--- admin/admin.js
+++ admin/admin.js
@@ -1,3 +1,4 @@
+//code for the forms to add and delete items from the lab
 document.addEventListener('DOMContentLoaded', function () {
     const addButton = document.getElementById('add-button');
 
@@ -44,5 +45,39 @@ document.addEventListener('DOMContentLoaded', function () {
 
         const formContainer = document.getElementById('add-form');
         formContainer.style.display = "none";
+    });
+
+    const removeButton = document.getElementById('remove-button');
+
+    removeButton.addEventListener('click', function() {
+        const deleteForm = document.getElementById('delete-item');
+        deleteForm.style.display = "flex";
+
+        const deleteTitle = document.getElementById('delete-title');
+        const itemTitleContainer = document.getElementById('item-title-text');
+        const itemTitle = itemTitleContainer.textContent;
+        deleteTitle.textContent = "Delete " + itemTitle;
+    });
+
+    const deleteButton = document.getElementById('delete-button');
+    deleteButton.addEventListener('click', function() {
+        const itemTitleContainer = document.getElementById('item-title-text');
+        const itemTitle = itemTitleContainer.textContent;
+
+        const labName = document.getElementById('lab-name').textContent;
+
+        // Insert PHP to find the specific item from the lab name and remove it
+
+        console.log("Lab to check:", labName);
+        console.log("Item to delete: ", itemTitle);
+
+        const deleteContainer = document.getElementById('delete-item');
+        deleteContainer.style.display = "none";
+    });
+
+    const cancelButton = document.getElementById('cancel-delete-button');
+    cancelButton.addEventListener('click', function() {
+        const deleteContainer = document.getElementById('delete-item');
+        deleteContainer.style.display = "none";
     });
 });
