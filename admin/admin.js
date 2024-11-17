52bf173849d153d04b81f7b804902053bfd75224
CMarink <christianm7765@gmail.com>
2024-11-17 17:52:17 -0500

    Fixed Add and Remove forms
    - Made both of these forms use async functions for the backend
    - All functionality should be the same as before.

--- admin/admin.js
+++ admin/admin.js
@@ -1,83 +1,100 @@
 //code for the forms to add and delete items from the lab
-document.addEventListener('DOMContentLoaded', function () {
-    const addButton = document.getElementById('add-button');
+async function add_form() {
+    const addForm = document.getElementById('add-form');
+    addForm.style.display = "flex";
 
-    addButton.addEventListener('click', function() {
-        const addForm = document.getElementById('add-form');
-        addForm.style.display = "flex";
-    });
-
-    const addForm = document.getElementById('add-form-object');
-    addForm.addEventListener('submit', function (event) {
+    addForm.onsubmit = async function(event) {
         event.preventDefault();
+        await add_item(event);
+    };
 
-        const labName = document.getElementById('lab-name').textContent;
-        const itemName = document.getElementById('item-name').value;
-        const itemType = document.getElementById('type-dropdown').value;
-        const itemDescription = document.getElementById('new-item-description').value;
-        const itemStock = document.getElementById('item-quantity').value;
-        const itemImage = document.getElementById('item-image').value;
+    const cancelAddButton = document.getElementById('cancel-add-form-button');
+    cancelAddButton.onclick = async function() { await cancel_add(); };
+}
 
-        if (itemName.length > 50) {
-            alert("The item name must be less than 100 charecters!")
-        }
-        else {
-            const formContainer = document.getElementById('add-form');
-            formContainer.style.display = "none";
+async function add_item(event) {
+    event.preventDefault();
 
-            // Fetch and POST to PHP here
+    const labName = document.getElementById('lab-name').textContent;
+    const itemName = document.getElementById('item-name').value;
+    const itemType = document.getElementById('type-dropdown').value;
+    const itemDescription = document.getElementById('new-item-description').value;
+    const itemStock = document.getElementById('item-quantity-input').value;
+    const itemImage = document.getElementById('item-image').value;
 
-            console.log("Lab Name: ", labName);
-            console.log("Item Name: ", itemName);
-            console.log("Item Type: ", itemType);
-            console.log("Item Description: ", itemDescription);
-            console.log("Item Stock: ", itemStock);
-            console.log("Item Image: ", itemImage);
-
-            addForm.reset();
-        }
-    });
-
-    const cancelAddFormButton = document.getElementById('cancel-add-form-button');
-    cancelAddFormButton.addEventListener('click', function () {
-        const addForm = document.getElementById('add-form-object');
-        addForm.reset();
-
+    if (itemName.length > 50) {
+        alert("The item name must be less than 100 charecters!")
+    }
+    else {
         const formContainer = document.getElementById('add-form');
         formContainer.style.display = "none";
-    });
 
-    const removeButton = document.getElementById('remove-button');
+        // Fetch and POST to PHP here
 
-    removeButton.addEventListener('click', function() {
-        const deleteForm = document.getElementById('delete-item');
-        deleteForm.style.display = "flex";
+        console.log("Lab Name: ", labName);
+        console.log("Item Name: ", itemName);
+        console.log("Item Type: ", itemType);
+        console.log("Item Description: ", itemDescription);
+        console.log("Item Stock: ", itemStock);
+        console.log("Item Image: ", itemImage);
 
-        const deleteTitle = document.getElementById('delete-title');
-        const itemTitleContainer = document.getElementById('item-title-text');
-        const itemTitle = itemTitleContainer.textContent;
-        deleteTitle.textContent = "Delete " + itemTitle;
-    });
+        const addForm = document.getElementById('add-form-object');
+        addForm.reset();
+    }
+}
+
+async function cancel_add() {
+    const addForm = document.getElementById('add-form-object');
+    addForm.reset();
+
+    const formContainer = document.getElementById('add-form');
+    formContainer.style.display = "none";
+}
+
+async function delete_form() {
+    const deleteForm = document.getElementById('delete-item');
+    deleteForm.style.display = "flex";
+
+    const deleteTitle = document.getElementById('delete-title');
+    const itemTitleContainer = document.getElementById('item-title-text');
+    const itemTitle = itemTitleContainer.textContent;
+    deleteTitle.textContent = "Delete " + itemTitle;
 
     const deleteButton = document.getElementById('delete-button');
-    deleteButton.addEventListener('click', function() {
-        const itemTitleContainer = document.getElementById('item-title-text');
-        const itemTitle = itemTitleContainer.textContent;
+    deleteButton.onclick = async function() { await delete_item(); };
 
-        const labName = document.getElementById('lab-name').textContent;
+    const cancelDeleteButton = document.getElementById('cancel-delete-button');
+    cancelDeleteButton.onclick = async function() { await cancel_delete(); };
+}
 
-        // Insert PHP to find the specific item from the lab name and remove it
+async function delete_item() {
+    const itemTitleContainer = document.getElementById('item-title-text');
+    const itemTitle = itemTitleContainer.textContent;
 
-        console.log("Lab to check:", labName);
-        console.log("Item to delete: ", itemTitle);
+    const labName = document.getElementById('lab-name').textContent;
 
-        const deleteContainer = document.getElementById('delete-item');
-        deleteContainer.style.display = "none";
-    });
+    // Insert PHP to find the specific item from the lab name and remove it
 
-    const cancelButton = document.getElementById('cancel-delete-button');
-    cancelButton.addEventListener('click', function() {
-        const deleteContainer = document.getElementById('delete-item');
-        deleteContainer.style.display = "none";
-    });
-});
+    console.log("Lab to check:", labName);
+    console.log("Item to delete: ", itemTitle);
+
+    const deleteContainer = document.getElementById('delete-item');
+    deleteContainer.style.display = "none";
+}
+
+async function cancel_delete() {
+    const deleteContainer = document.getElementById('delete-item');
+    deleteContainer.style.display = "none";
+}
+
+// THESE SHOULD BE MOVED TO Lablist.js UNDER populate_item_details() WHEN MERGED WITH BACKEND!
+// Should be right below the call for checkout form in the current backend branch
+const addButtonContainer = document.getElementById('add-button');
+addButtonContainer.onclick = async function() {
+    add_form();
+};
+
+const removeButtonContainer = document.getElementById('remove-button');
+removeButtonContainer.onclick = async function() {
+    delete_form();
+};
