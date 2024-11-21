9cc03b0f3d36db4ec528c316a279161337ab12e6
kellieng <122571116+kellieng@users.noreply.github.com>
2024-11-21 00:09:47 -0500

    Admin Add Items PHP

---
+++ backend/queries/admin_addItem.php
@@ -0,0 +1,27 @@
+<?php
+    require "db.php";
+
+    if ($isset($_GET["itemName"], $_GET["itemType"], $_GET["itemStock"], $_GET["labName"])) {
+        $name = $_GET["itemName"];
+        $borrowable = $_GET["itemType"]; // borrowable
+        $stock = $_GET["itemStock"];
+        $location_name = $_GET["labName"];
+
+    } else {
+        return;
+    }
+    if (isset($_GET["itemDescription"])) {
+        $item_desc = $_GET["itemDescription"]; // optional
+    } else {
+        $item_desc = NULL;
+    }
+    if (isset($_GET["itemImage"])) {
+        $image = $_GET["itemImage"]; // optional
+    } else {
+        $image = NULL;
+    }
+
+    $result = db->prepare("INSERT INTO items (`name`, `borrowable`, `description`, `stock`, `image_link`, `location_name`) VALUES (?, ?, ?, ?, ?, ?)");
+    $result->bind_param("siisss", $name, $borrowable, $item_desc, $stock, $image, $location_name);
+    $result->execute();
+?>
